22f1cd735c660a9fe96c2cde83a68bbadd7da872
Alex L <118956261+Luckyleck@users.noreply.github.com>
2023-10-12 12:41:44 -0400

    Time: 145 ms (45.21%), Space: 51.8 MB (9.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,18 +2,6 @@
  * @param {number} x
  * @return {boolean}
  */
-// var isPalindrome = function(x) {
-    
-//     let normalOrder = x.toString().split("");
-//     let reverseOrder = [];
-
-//     for (let num of normalOrder) {
-//         reverseOrder.unshift(num);
-//     }
-
-//     return normalOrder.join("") === reverseOrder.join("");
-// };
-
 const isPalindrome = (num) => {
     return String(num) === String(num).split('').reverse().join('')
 }
