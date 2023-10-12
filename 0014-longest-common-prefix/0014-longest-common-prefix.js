1cbd47b4241edccd71a0ad7ba3e07abb221a938c
Alex L <118956261+Luckyleck@users.noreply.github.com>
2023-10-12 12:59:41 -0400

    Time: 69 ms (6.61%), Space: 41.8 MB (88.26%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -7,16 +7,27 @@ function longestCommonPrefix(strs) {
     return "";
   }
 
-  let prefix = strs[0];
+  return findCommonPrefix(strs, 0, strs.length - 1);
+}
 
-  for (let i = 1; i < strs.length; i++) {
-    while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      console.log(prefix)
-      if (prefix === "") {
-        return "";
-      }
+function findCommonPrefix(strs, left, right) {
+  if (left === right) {
+    return strs[left];
+  }
+
+  const mid = Math.floor((left + right) / 2);
+  const prefix1 = findCommonPrefix(strs, left, mid);
+  const prefix2 = findCommonPrefix(strs, mid + 1, right);
+
+  return commonPrefix(prefix1, prefix2);
+}
+
+function commonPrefix(str1, str2) {
+  const minLength = Math.min(str1.length, str2.length);
+  for (let i = 0; i < minLength; i++) {
+    if (str1[i] !== str2[i]) {
+      return str1.substring(0, i);
     }
   }
-  return prefix;
+  return str1.substring(0, minLength);
 }
