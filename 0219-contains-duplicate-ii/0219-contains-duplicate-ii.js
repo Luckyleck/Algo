72bc7998e2d4ca82dfb564937123d159c45ab1fa
Alex L <118956261+Luckyleck@users.noreply.github.com>
2023-11-06 19:53:30 -0500

    Time: 7071 ms (5%), Space: 50.4 MB (85.18%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (Math.abs(i - j) <= k) {
+                if (nums[i] === nums[j]) {
+                    return true
+                }
+            }
+        }
+    }
+
+    return false;
+};
