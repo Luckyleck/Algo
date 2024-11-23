ae5c4d03581f44174e6524a5d6954cc85f09ccf0
Alex L <118956261+Luckyleck@users.noreply.github.com>
2024-11-22 21:13:51 -0500

    Time: 54 ms (95.37%), Space: 49.7 MB (22.82%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,44 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let timeoutId;
+
+    // Schedule the execution of the function after `t` milliseconds
+    timeoutId = setTimeout(() => {
+        const result = fn(...args);
+        console.log({ time: t, returned: result });
+    }, t);
+
+    // Return a cancel function to clear the timeout
+    return function cancelFn() {
+        clearTimeout(timeoutId);
+    };
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
