7d213cadfd6a02b944f274b3e026d7cc29673d94
Alex L <118956261+Luckyleck@users.noreply.github.com>
2024-12-10 15:15:32 +0100

    Time: 62 ms (24.25%), Space: 52.2 MB (16.51%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -11,14 +11,12 @@ var chunk = function(arr, size) {
     for (let i = 0; i < arr.length; i++) {
         tempArray.push(arr[i]);
 
-        // If the tempArray size reaches the chunk size, push it to finalArray and reset tempArray
         if (tempArray.length === size) {
             finalArray.push(tempArray);
             tempArray = [];
         }
     }
 
-    // Add the remaining items in tempArray to finalArray
     if (tempArray.length > 0) {
         finalArray.push(tempArray);
     }
